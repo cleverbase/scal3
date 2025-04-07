b441d52219856feb8ba19bfa6a3a493d16918372
Sander Dijkhuis <mail@sanderdijkhuis.nl>
2025-04-07 10:18:57 +0200

    feat: simpler FFI interface based on CBOR

---
+++ demo.scala
@@ -0,0 +1,246 @@
+//> using scala 3.7.0
+//> using dependency "net.java.dev.jna:jna:5.17.0"
+//> using dependency "io.bullet::borer-core:1.16.1"
+//> using dependency "io.bullet::borer-derivation:1.16.1"
+//> using dependency "org.bouncycastle:bcprov-jdk18on:1.81"
+
+import com.sun.jna.ptr.{IntByReference, PointerByReference}
+import com.sun.jna.{Library, Native, Pointer}
+import io.bullet.borer.*
+import io.bullet.borer.derivation.MapBasedCodecs.*
+import io.bullet.borer.derivation.key
+import org.bouncycastle.jce.interfaces.{ECPrivateKey, ECPublicKey}
+import org.bouncycastle.jce.provider.BouncyCastleProvider
+import org.bouncycastle.jce.spec.ECPublicKeySpec
+
+import java.security.{KeyFactory, KeyPairGenerator, MessageDigest, SecureRandom, Security, Signature}
+import javax.crypto.{KeyAgreement, KeyGenerator, Mac, SecretKey}
+import scala.util.Try
+
+val libraryPath = "target/release/libscal3.so"
+
+object authentication:
+  private object impl:
+    private trait AuthenticationLibrary extends Library:
+      def scal3_process(ip: Array[Byte], il: Int, op: PointerByReference, ol: IntByReference): Unit
+      def scal3_free(ptr: Pointer, len: Int): Unit
+
+    private val library: AuthenticationLibrary = Native.load(libraryPath, classOf[AuthenticationLibrary])
+
+    given AdtEncodingStrategy = AdtEncodingStrategy.flat(typeMemberName = "type")
+
+    sealed trait Request derives Codec
+    object Request:
+      @key("register") case class Register(mask: Array[Byte], randomness: Array[Byte], provider: Array[Byte])
+          extends Request derives Codec
+      @key("accept") case class Accept(
+          provider: Array[Byte],
+          verifierSecret: Array[Byte],
+          verifier: Array[Byte],
+          device: Array[Byte]
+      ) extends Request
+          derives Codec
+      @key("challenge") case class Challenge(randomness: Array[Byte]) extends Request derives Codec
+      @key("authenticate") case class Authenticate(
+          mask: Array[Byte],
+          randomness: Array[Byte],
+          provider: Array[Byte],
+          subscriber: Array[Byte],
+          verifier: Array[Byte],
+          device: Array[Byte],
+          challenge: Array[Byte],
+          hash: Array[Byte]
+      ) extends Request
+          derives Codec
+      @key("pass") case class Pass(proof: Array[Byte], authentication: Long) extends Request derives Codec
+      @key("prove") case class Prove(
+          randomness: Array[Byte],
+          provider: Array[Byte],
+          verifierSecret: Array[Byte],
+          verifier: Array[Byte],
+          device: Array[Byte],
+          hash: Array[Byte],
+          passSecret: Array[Byte],
+          pass: Array[Byte]
+      ) extends Request
+          derives Codec
+      @key("verify") case class Verify(
+          verifier: Array[Byte],
+          device: Array[Byte],
+          hash: Array[Byte],
+          authenticator: Array[Byte],
+          proof: Array[Byte],
+          client: Array[Byte]
+      ) extends Request
+          derives Codec
+
+    def invoke[Response](request: Request)(using Decoder[Response]): Try[Response] =
+      val input = Cbor.encode(request).toByteArray
+      val outputPtr = PointerByReference()
+      val outputLen = IntByReference()
+      impl.library.scal3_process(input, input.length, outputPtr, outputLen)
+      val output =
+        try outputPtr.getValue.getByteArray(0, outputLen.getValue)
+        finally impl.library.scal3_free(outputPtr.getValue, outputLen.getValue)
+      Cbor.decode(output).to[Response].valueTry
+
+  object domain:
+    import impl.*
+
+    case class SubscriberState(mask: Array[Byte], randomness: Array[Byte], provider: ECPublicKey)
+    case class ProviderState(provider: ECPublicKey, secret: Array[Byte], verifier: Array[Byte], device: ECPublicKey)
+    case class Registration(subscriber: Array[Byte], verifier: Array[Byte]) derives Codec
+    case class PassResponse(sender: Array[Byte], pass: Array[Byte]) derives Codec
+    case class Transcript(authenticator: Array[Byte], proof: Array[Byte], client: Array[Byte]) derives Codec
+    object service:
+      def register(state: SubscriberState): Registration = invoke[Registration](
+        Request.Register(state.mask, state.randomness, state.provider.getQ.getEncoded(true))
+      ).get
+      def accept(state: ProviderState): Boolean =
+        invoke[String](
+          Request.Accept(
+            state.provider.getQ.getEncoded(true),
+            state.secret,
+            state.verifier,
+            state.device.getQ.getEncoded(true)
+          )
+        ).get == "accepted"
+      def challenge(randomness: Array[Byte]): Array[Byte] = {
+        case class Response(challenge: Array[Byte]) derives Codec
+        invoke[Response](Request.Challenge(randomness)).get.challenge
+      }
+      def authenticate(
+          state: SubscriberState,
+          registration: Registration,
+          device: ECPublicKey,
+          challenge: Array[Byte],
+          hash: Array[Byte]
+      ): (Array[Byte], Array[Byte] => PassResponse) = {
+        case class AuthenticateResponse(digest: Array[Byte], authentication: Long) derives Codec
+        val response = invoke[AuthenticateResponse](
+          Request.Authenticate(
+            state.mask,
+            state.randomness,
+            state.provider.getQ.getEncoded(true),
+            registration.subscriber,
+            registration.verifier,
+            device.getQ.getEncoded(true),
+            challenge,
+            hash
+          )
+        ).get
+        def pass(proof: Array[Byte]): PassResponse =
+          invoke[PassResponse](Request.Pass(proof, response.authentication)).get
+        (response.digest, pass)
+      }
+      def prove(
+          randomness: Array[Byte],
+          state: ProviderState,
+          hash: Array[Byte],
+          passSecret: Array[Byte],
+          pass: Array[Byte]
+      ): Transcript =
+        invoke[Transcript](
+          Request.Prove(
+            randomness,
+            state.provider.getQ.getEncoded(true),
+            state.secret,
+            state.verifier,
+            state.device.getQ.getEncoded(true),
+            hash,
+            passSecret,
+            pass
+          )
+        ).get
+      def verify(verifier: Array[Byte], device: ECPublicKey, hash: Array[Byte], transcript: Transcript): Boolean =
+        invoke[String](
+          Request.Verify(
+            verifier,
+            device.getQ.getEncoded(true),
+            hash,
+            transcript.authenticator,
+            transcript.proof,
+            transcript.client
+          )
+        ).get == "verified"
+
+object crypto:
+  object domain:
+    def generateRandomness(): Array[Byte] = {
+      val randomness = Array.ofDim[Byte](32)
+      SecureRandom().nextBytes(randomness)
+      randomness
+    }
+
+    case class HmacSha256Key(k: SecretKey):
+      def hmac(msg: Array[Byte]): Array[Byte] =
+        val mac = Mac.getInstance("HmacSHA256")
+        mac.init(k)
+        mac.doFinal(msg)
+    object HmacSha256Key:
+      def generate(): HmacSha256Key =
+        val generator = KeyGenerator.getInstance("HmacSHA256")
+        generator.init(256)
+        HmacSha256Key(generator.generateKey())
+
+    case class P256Key(sk: ECPrivateKey, pk: ECPublicKey):
+      def ecdh(pk: Array[Byte]): Array[Byte] =
+        val params = sk.getParameters
+        val curve = params.getCurve
+        val point = curve.decodePoint(pk)
+        val spec = ECPublicKeySpec(point, params)
+        val factory = KeyFactory.getInstance("EC", "BC")
+        val other = factory.generatePublic(spec)
+        val agreement = KeyAgreement.getInstance("ECDH", "BC")
+        agreement.init(sk)
+        agreement.doPhase(other, true)
+        agreement.generateSecret()
+      def ecdsa(digest: Array[Byte]): Array[Byte] =
+        val signature = Signature.getInstance("NONEwithECDSAinP1363format")
+        signature.initSign(sk)
+        signature.update(digest)
+        signature.sign()
+
+    object P256Key:
+      def generate(): P256Key =
+        val generator = KeyPairGenerator.getInstance("EC", "BC")
+        generator.initialize(256)
+        val pair = generator.generateKeyPair()
+        P256Key(pair.getPrivate.asInstanceOf[ECPrivateKey], pair.getPublic.asInstanceOf[ECPublicKey])
+
+    case class SecureArea(ec: P256Key, secret: HmacSha256Key)
+
+    def sha256(msg: Array[Byte]): Array[Byte] = MessageDigest.getInstance("SHA-256").digest(msg)
+
+@main def main(): Unit = {
+  import authentication.domain.*, crypto.domain.*
+
+  Security.addProvider(BouncyCastleProvider())
+
+  val provider = SecureArea(P256Key.generate(), HmacSha256Key.generate())
+  val device = SecureArea(P256Key.generate(), HmacSha256Key.generate())
+
+  val subscriberState = SubscriberState(device.secret.hmac("123456".getBytes), generateRandomness(), provider.ec.pk)
+  val registration = service.register(subscriberState)
+  val providerState =
+    ProviderState(provider.ec.pk, provider.ec.ecdh(registration.subscriber), registration.verifier, device.ec.pk)
+  assert(service.accept(providerState))
+
+  val challengeData = """{"nonce":"123456","timestamp":"1748886705"}""".getBytes
+  val challenge = service.challenge(provider.secret.hmac(challengeData))
+  val clientData = """{"operation":"log-in","session":"68c9eeeddfa5fb50"}""".getBytes
+  val (digest, pass) = service.authenticate(subscriberState, registration, device.ec.pk, challenge, sha256(clientData))
+  val proof = device.ec.ecdsa(digest)
+  val result = pass(proof)
+  val transcript = service.prove(
+    provider.secret.hmac(challengeData),
+    providerState,
+    sha256(clientData),
+    provider.ec.ecdh(result.sender),
+    result.pass
+  )
+
+  assert(service.verify(registration.verifier, device.ec.pk, sha256(clientData), transcript))
+
+  println("Demo completed successfully")
+}
--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,53 +1,53 @@
-pub mod provider;
-pub mod subscriber;
-
-use crate::program;
+use crate::domain::Authentication;
+use crate::handle::insert_authentication;
+use crate::{domain, program};
+use serde::{Deserialize, Serialize};
 
 /// Enrolled verification data for the [subscriber].
 ///
 /// Contains a joint verifying key, and two encrypted signing shares.
-pub type Verifier = [u8; 32 + 33 + 33 + 136 + 16];
+pub(crate) type Verifier = [u8; 32 + 33 + 33 + 136 + 16];
 
 /// Public key for key agreement or signing.
 ///
 /// Contains a compressed P-256 point.
-pub type Key = [u8; 33];
+pub(crate) type Key = [u8; 33];
 
 /// Authenticator-generated verification data.
 ///
 /// Contains the first part of a joint signature and an authentication tag
 /// protecting integrity of the [subscriber]’s signature share.
-pub type Authenticator = [u8; 32 + 16];
+pub(crate) type Authenticator = [u8; 32 + 16];
 
 /// Verification data proving authenticator possession.
 ///
 /// Contains an ECDSA P-256 device signature.
-pub type Proof = [u8; 64];
+pub(crate) type Proof = [u8; 64];
 
 /// Client-generated verification data.
 ///
 /// Contains a P-256 binding public key, an ECDSA P-256 binding signature,
 /// and the second part of a joint signature.
-pub type Client = [u8; 33 + 64 + 32];
+pub(crate) type Client = [u8; 33 + 64 + 32];
 
 /// Secret entropy derived from execution context data in a secure area.
-pub type Randomness = [u8; 32];
+pub(crate) type Randomness = [u8; 32];
 
 /// Shared secret between [provider] and alleged [subscriber].
 ///
 /// Contains the P-256 ECDH shared secret generated between server and
 /// client: the x-coordinate of the scalar-point multiplication.
-pub type Secret = [u8; 32];
+pub(crate) type Secret = [u8; 32];
 
 /// Secret entropy derived from application-provided data in a secure area.
 ///
 /// Gets added to the subscriber signing share in the [Verifier].
-pub type Mask = [u8; 32];
+pub(crate) type Mask = [u8; 32];
 
 /// Authentication challenge data.
 ///
 /// Contains [provider] commitments ([<i>d</i><sub>1</sub>]<i>G</i>, [<i>e</i><sub>1</sub>]<i>G</i>).
-pub type Challenge = [u8; 33 + 33];
+pub(crate) type Challenge = [u8; 33 + 33];
 
 /// Response to a [Challenge].
 ///
@@ -55,170 +55,325 @@ pub type Challenge = [u8; 33 + 33];
 /// ([<i>d</i><sub>2</sub>]<i>G</i>, [<i>e</i><sub>2</sub>]<i>G</i>),
 /// encrypted [subscriber] signature share <i>z</i><sub>2</sub>], a device signature, and a binding
 /// signature.
-pub type Pass = [u8; 33 + 33 + 64 + 33 + 64 + 33 + 32 + 16];
+pub(crate) type Pass = [u8; 33 + 33 + 64 + 33 + 64 + 33 + 32 + 16];
 
 /// Hash digest representing client data or device-signed data.
 ///
 /// Contains a SHA-256 hash digest.
-pub type Digest = [u8; 32];
+pub(crate) type Digest = [u8; 32];
 
-/// Verifies evidence that the identified [subscriber] passed the [Digest].
-#[export_name = "scal3_verify"]
-pub extern "C" fn verify(
-    verifier: &Verifier,
-    pk_device: &Key,
-    client_data_hash: &Digest,
-    authenticator: &Authenticator,
-    proof: &Proof,
-    client: &Client,
-) -> bool {
-    program::verify(
-        verifier,
-        pk_device,
-        client_data_hash,
-        authenticator,
-        proof,
-        client,
-    )
-    .is_ok()
+#[derive(Serialize)]
+#[serde(rename_all = "camelCase")]
+pub enum Error {
+    BadRequest,
+    MissingValue,
+    InvalidInput,
 }
 
-#[cfg(test)]
-mod test {
-    use crate::api::*;
-    use hmac::digest::{crypto_common, KeyInit};
-    use hmac::{Hmac, Mac};
-    use p256::elliptic_curve::sec1::ToEncodedPoint;
-    use p256::elliptic_curve::{PublicKey, SecretKey};
-    use p256::{NistP256, NonZeroScalar};
-    use sha2::{Digest as Sha2Digest, Sha256};
-    use signature::hazmat::PrehashSigner;
-    use std::ptr::null_mut;
-    use signature::rand_core::{OsRng, RngCore};
+#[derive(Serialize)]
+pub(crate) struct ErrorResponse {
+    pub(crate) error: Error,
+}
 
-    #[test]
-    fn example() {
-        fn sec1_compressed(pk: PublicKey<NistP256>) -> Key {
-            pk.to_encoded_point(true).as_ref().try_into().unwrap()
-        }
+#[derive(Serialize)]
+#[serde(untagged)]
+pub(crate) enum Response {
+    Challenge(ChallengeResponse),
+    Register(Option<Registration>),
+    Accept(Acceptance),
+    Authenticate(AuthenticateResponse2),
+    Pass(Attempt),
+    Prove(Option<Transcript>),
+    Verify(Verification),
+    Error(ErrorResponse),
+}
 
-        fn prf(k: &crypto_common::Key<Hmac<Sha256>>, msg: &[u8]) -> [u8; 32] {
-            <Hmac<Sha256> as Mac>::new(k)
-                .chain_update(msg)
-                .finalize()
-                .into_bytes()
-                .try_into()
-                .unwrap()
-        }
+#[derive(Serialize, Deserialize)]
+pub(crate) struct SubscriberState {
+    #[serde(with = "serde_bytes", default = "Option::default")]
+    mask: Option<Mask>,
+    #[serde(with = "serde_bytes", default = "Option::default")]
+    randomness: Option<Randomness>,
+    #[serde(with = "serde_bytes", default = "Option::default")]
+    provider: Option<Key>,
+}
 
-        fn ecdh(sk: &SecretKey<NistP256>, pk: &[u8; 33]) -> Secret {
-            let sk = NonZeroScalar::from_repr(sk.to_bytes()).unwrap();
-            let pk = p256::PublicKey::from_sec1_bytes(pk).unwrap();
-            let secret = p256::ecdh::diffie_hellman::<NistP256>(sk, pk.as_affine());
-            let bytes = secret.raw_secret_bytes().clone();
-            bytes.into()
-        }
-
-        fn sign_prehash(sk: &p256::ecdsa::SigningKey, hash: &Digest, proof: &mut Proof) {
-            let (signature, _) = sk.sign_prehash(hash).unwrap();
-            proof.copy_from_slice(&signature.to_bytes());
-        }
-
-        let mut randomness = [0u8; size_of::<Randomness>()];
-        let mut subscriber = [0u8; size_of::<Key>()];
-        let mut verifier = [0u8; size_of::<Verifier>()];
-        let mut challenge = [0u8; size_of::<Challenge>()];
-        let mut mask = [0u8; size_of::<Mask>()];
-        let mut client_data_hash = [0u8; size_of::<Digest>()];
-        let mut to_sign = [0u8; size_of::<Digest>()];
-        let mut proof = [0u8; size_of::<Proof>()];
-        let mut sender = [0u8; size_of::<Key>()];
-        let mut pass = [0u8; size_of::<Pass>()];
-        let mut authenticator = [0u8; size_of::<Authenticator>()];
-        let mut client = [0u8; size_of::<Client>()];
-
-        // Setup
-
-        // Provider keys would be protected by a hardware security module
-        let sk_provider = p256::SecretKey::random(&mut OsRng);
-        let pk_provider = sec1_compressed(sk_provider.public_key());
-        let k_provider = Hmac::<Sha256>::generate_key(&mut OsRng);
-
-        // Enrolment
-
-        // Subscriber keys would be protected by a local secure area, e.g. a StrongBox Keymaster
-        let sk_subscriber = p256::ecdsa::SigningKey::random(&mut OsRng);
-        let pk_subscriber = sec1_compressed(sk_subscriber.verifying_key().into());
-        let k_subscriber = Hmac::<Sha256>::generate_key(&mut OsRng);
-
-        // The subscriber derives a mask from a PIN, e.g. using a local hardware-backed PRF
-        mask.copy_from_slice(&prf(&k_subscriber, b"123456"));
-        OsRng.fill_bytes(&mut randomness);
-        subscriber::register(
-            &mask,
-            &randomness,
-            &pk_provider,
-            &mut subscriber,
-            &mut verifier,
-        );
-
-        assert!(provider::accept(
-            &pk_provider,
-            &ecdh(&sk_provider, &subscriber),
-            &verifier
-        ));
-
-        // Authentication
-
-        let challenge_data = b"ts=1743930934&nonce=000001";
-        randomness.copy_from_slice(&prf(&k_provider, challenge_data));
-        provider::challenge(&randomness, &mut challenge);
-
-        mask.copy_from_slice(&prf(&k_subscriber, b"123456"));
-        OsRng.fill_bytes(&mut randomness);
-        let client_data = b"{\"operation\":\"log-in\",\"session\":\"68c9eeeddfa5fb50\"}";
-        client_data_hash.copy_from_slice(Sha256::digest(client_data).as_slice());
-        let authentication = subscriber::authenticate(
-            &mask,
-            &randomness,
-            &pk_provider,
-            &subscriber,
-            &verifier,
-            &challenge,
-            &client_data_hash,
-            &mut to_sign,
-        );
-        assert_ne!(null_mut(), authentication);
-        sign_prehash(&sk_subscriber, &to_sign, &mut proof);
-        assert!(subscriber::pass(
-            authentication,
-            &proof,
-            &mut sender,
-            &mut pass
-        ));
-
-        randomness.copy_from_slice(&prf(&k_provider, challenge_data));
-        assert!(provider::prove(
-            &randomness,
-            &pk_provider,
-            &ecdh(&sk_provider, &subscriber),
-            &verifier,
-            &pk_subscriber,
-            &client_data_hash,
-            &ecdh(&sk_provider, &sender),
-            &pass,
-            &mut authenticator,
-            &mut proof,
-            &mut client
-        ));
-
-        assert!(verify(
-            &verifier,
-            &pk_subscriber,
-            &client_data_hash,
-            &authenticator,
-            &proof,
-            &client
-        ));
+impl SubscriberState {
+    pub(crate) fn handle(&self) -> Option<Option<Registration>> {
+        Some(
+            program::subscriber::register(&self.mask?, &self.randomness?, &self.provider?).map(
+                |(key, verifier)| Registration {
+                    subscriber: Some(key),
+                    verifier: Some(verifier),
+                },
+            ),
+        )
     }
+}
+
+#[derive(Serialize, Deserialize)]
+pub(crate) struct Registration {
+    #[serde(with = "serde_bytes", default = "Option::default")]
+    subscriber: Option<Key>,
+    #[serde(with = "serde_bytes", default = "Option::default")]
+    verifier: Option<Verifier>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub(crate) struct RegisterResponse {
+    #[serde(flatten)]
+    registration: Option<Registration>,
+    #[serde(default = "Option::default", skip_serializing_if = "Option::is_none")]
+    error: Option<String>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub(crate) struct AuthenticateRequest {
+    #[serde(flatten)]
+    state: SubscriberState,
+    #[serde(with = "serde_bytes", default = "Option::default")]
+    subscriber: Option<Key>,
+    #[serde(flatten)]
+    credential: Credential,
+    #[serde(with = "serde_bytes", default = "Option::default")]
+    challenge: Option<Challenge>,
+    #[serde(with = "serde_bytes", default = "Option::default")]
+    hash: Option<Digest>,
+}
+
+#[derive(Serialize)]
+pub(crate) struct AuthenticateResponse2 {
+    #[serde(with = "serde_bytes")]
+    digest: Digest,
+    authentication: u64,
+}
+
+impl AuthenticateRequest {
+    pub(crate) fn handle(&self) -> Option<Option<(Authentication, Digest)>> {
+        Some(program::subscriber::authenticate(
+            &self.state.mask?,
+            &self.state.randomness?,
+            &self.state.provider?,
+            &self.subscriber?,
+            &self.credential.verifier?,
+            &self.challenge?,
+            &self.hash?,
+        ))
+    }
+
+    pub(crate) fn handle2(&self) -> Option<AuthenticateResponse2> {
+        match self.handle() {
+            None => None,
+            Some(None) => None,
+            Some(Some((authentication, digest))) => {
+                let authentication_id = insert_authentication(authentication);
+                Some(AuthenticateResponse2 {
+                    digest,
+                    authentication: authentication_id,
+                })
+            }
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+pub(crate) struct AuthenticateResponse {
+    #[serde(
+        with = "serde_bytes",
+        default = "Option::default",
+        skip_serializing_if = "Option::is_none"
+    )]
+    digest: Option<Digest>,
+    #[serde(default = "Option::default", skip_serializing_if = "Option::is_none")]
+    error: Option<String>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub(crate) struct PassRequest {
+    #[serde(with = "serde_bytes", default = "Option::default")]
+    proof: Option<Proof>,
+    #[serde(default = "Option::default")]
+    pub(crate) authentication: Option<u64>,
+}
+
+impl PassRequest {
+    pub(crate) fn handle(&self, authentication: Authentication) -> Option<Option<(Key, Pass)>> {
+        Some(program::subscriber::pass(authentication, &self.proof?))
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+pub(crate) struct Attempt {
+    #[serde(with = "serde_bytes", default = "Option::default")]
+    pub(crate) sender: Option<Key>,
+    #[serde(with = "serde_bytes", default = "Option::default")]
+    pub(crate) pass: Option<Pass>,
+}
+
+#[derive(Serialize, Deserialize)]
+struct PassResponse {
+    #[serde(flatten)]
+    attempt: Option<Attempt>,
+    #[serde(default = "Option::default", skip_serializing_if = "Option::is_none")]
+    error: Option<String>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub(crate) struct ChallengeRequest {
+    #[serde(with = "serde_bytes", default = "Option::default")]
+    randomness: Option<Randomness>,
+}
+
+impl ChallengeRequest {
+    pub(crate) fn handle(&self) -> Option<Challenge> {
+        Some(program::provider::challenge(&self.randomness?))
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+pub(crate) struct ChallengeResponse {
+    #[serde(
+        with = "serde_bytes",
+        default = "Option::default",
+        skip_serializing_if = "Option::is_none"
+    )]
+    pub(crate) challenge: Option<Challenge>,
+    #[serde(default = "Option::default", skip_serializing_if = "Option::is_none")]
+    pub(crate) error: Option<String>,
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+pub(crate) struct ProviderState {
+    #[serde(with = "serde_bytes", default = "Option::default")]
+    provider: Option<Key>,
+    #[serde(
+        with = "serde_bytes",
+        default = "Option::default",
+        rename = "verifierSecret"
+    )]
+    secret: Option<Secret>,
+    #[serde(flatten)]
+    credential: Credential,
+}
+
+impl ProviderState {
+    pub(crate) fn handle(&self) -> Option<domain::Result> {
+        Some(program::provider::accept(
+            &self.provider?,
+            &self.secret?,
+            &self.credential.verifier?,
+        ))
+    }
+}
+
+#[derive(Serialize)]
+#[serde(rename_all = "camelCase")]
+pub(crate) enum Acceptance {
+    Accepted,
+    Rejected,
+}
+
+#[derive(Serialize, Deserialize)]
+struct AcceptResponse {
+    #[serde(default = "Option::default", skip_serializing_if = "Option::is_none")]
+    result: Option<String>,
+    #[serde(default = "Option::default", skip_serializing_if = "Option::is_none")]
+    error: Option<String>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub(crate) struct ProveRequest {
+    #[serde(with = "serde_bytes", default = "Option::default")]
+    randomness: Option<Randomness>,
+    #[serde(flatten)]
+    state: ProviderState,
+    #[serde(with = "serde_bytes", default = "Option::default")]
+    hash: Option<Digest>,
+    #[serde(
+        with = "serde_bytes",
+        default = "Option::default",
+        rename = "passSecret"
+    )]
+    pass_secret: Option<Secret>,
+    #[serde(with = "serde_bytes", default = "Option::default")]
+    pass: Option<Pass>,
+}
+
+impl ProveRequest {
+    pub(crate) fn handle(&self) -> Option<Option<(Authenticator, Proof, Client)>> {
+        Some(program::provider::prove(
+            &self.randomness?,
+            &self.state.provider?,
+            &self.state.secret?,
+            &self.state.credential.verifier?,
+            &self.state.credential.device?,
+            &self.hash?,
+            &self.pass_secret?,
+            &self.pass?,
+        ))
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct ProveResponse {
+    #[serde(flatten)]
+    result: Option<Transcript>,
+    #[serde(default = "Option::default", skip_serializing_if = "Option::is_none")]
+    error: Option<String>,
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+struct Credential {
+    #[serde(with = "serde_bytes", default = "Option::default")]
+    verifier: Option<Verifier>,
+    #[serde(with = "serde_bytes", default = "Option::default")]
+    device: Option<Key>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub(crate) struct Transcript {
+    #[serde(with = "serde_bytes", default = "Option::default")]
+    pub(crate) authenticator: Option<Authenticator>,
+    #[serde(with = "serde_bytes", default = "Option::default")]
+    pub(crate) proof: Option<Proof>,
+    #[serde(with = "serde_bytes", default = "Option::default")]
+    pub(crate) client: Option<Client>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub(crate) struct VerifyRequest {
+    #[serde(flatten)]
+    credential: Credential,
+    #[serde(with = "serde_bytes", default = "Option::default")]
+    hash: Option<Digest>,
+    #[serde(flatten)]
+    transcript: Transcript,
+}
+
+impl VerifyRequest {
+    pub(crate) fn handle(&self) -> Option<domain::Result> {
+        Some(program::verify(
+            &self.credential.verifier?,
+            &self.credential.device?,
+            &self.hash?,
+            &self.transcript.authenticator?,
+            &self.transcript.proof?,
+            &self.transcript.client?,
+        ))
+    }
+}
+
+#[derive(Serialize)]
+#[serde(rename_all = "camelCase")]
+pub(crate) enum Verification {
+    Verified,
+    Rejected,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct VerifyResponse {
+    #[serde(default = "Option::default", skip_serializing_if = "Option::is_none")]
+    result: Option<String>,
+    #[serde(default = "Option::default", skip_serializing_if = "Option::is_none")]
+    error: Option<String>,
 }
